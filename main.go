6f70b718bc6715c2cb3d03a3b9cc237dff36025f
Patrick Jimison <pat.jimison@gmail.com>
2026-01-19 20:13:17 -0700

    Added /metrics endpoint for tracking api request counts and /reset endpoint for resetting the count

--- main.go
+++ main.go
@@ -5,16 +5,26 @@ import (
 	"fmt"
 	"net/http"
 	"log"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 	mux.HandleFunc("/healthz", handlerReadyCheck)
+	mux.HandleFunc("/metrics", apiCfg.handlerWriteRequestCount)
+	mux.HandleFunc("/reset", apiCfg.handlerResetRequestCount)
 
 	srv := http.Server {
 		Addr:		":" + port,
@@ -28,9 +38,27 @@ func main() {
 	}
 }
 
-func handlerReadyCheck(w http.ResponseWriter, req *http.Request){
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")	
+func handlerReadyCheck(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerWriteRequestCount(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) handlerResetRequestCount(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
 
