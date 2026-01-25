22bb015f76f2b0d93c46374b249f11c310f95adc
Patrick Jimison <pat.jimison@gmail.com>
2026-01-25 13:28:12 -0700

    Add handler for validating chirp length and cleaning bad words

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 	"log"
 	"sync/atomic"
+	"encoding/json"
+	"io"
+	"regexp"
 )
 
 type apiConfig struct {
@@ -24,6 +27,7 @@ func main() {
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 
 	mux.HandleFunc("GET /api/healthz", handlerReadyCheck)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerWriteRequestCount)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerResetRequestCount)
 
@@ -39,10 +43,68 @@ func main() {
 	}
 }
 
-func handlerReadyCheck(w http.ResponseWriter, req *http.Request) {
+func handlerReadyCheck(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type requestBody struct {
+		Body string `json:"body"`
+	}
+	type responseBody struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	dat, err := io.ReadAll(r.Body)
+	if err != nil { 
+		respondWithError(w, 500, "couldn't read request")
+		return
+	}
+
+	req := requestBody{}
+	err = json.Unmarshal(dat, &req)
+	if err != nil { 
+		respondWithError(w, 500, "couldn't unmarshal parameters")
+		return
+	}
+
+	origMsg := req.Body
+	if len(origMsg) > 140 { 
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	respondWithJSON(w, 200, responseBody{
+		CleanedBody: cleanMessage(origMsg),
+	})
+}
+
+func cleanMessage(msg string) string {
+	cleanedMsg := msg
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	for _, badWord := range badWords {
+		pattern := fmt.Sprintf("(?i)%s", badWord)
+		re := regexp.MustCompile(pattern)
+		cleanedMsg = re.ReplaceAllString(cleanedMsg, "****")
+	}
+	return cleanedMsg
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+	return nil
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) error { 
+	return respondWithJSON(w, code, map[string]string{"error": msg})
 }
 
 func (cfg *apiConfig) handlerWriteRequestCount(w http.ResponseWriter, req *http.Request) {
