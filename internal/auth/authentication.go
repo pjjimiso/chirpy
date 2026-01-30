8237f3448f9025bd7249d51d80e581fe79820973
Patrick Jimison <patrick.j.jimison@intel.com>
2026-01-29 17:17:15 -0700

    Added JWT creation and validation

--- internal/auth/authentication.go
+++ internal/auth/authentication.go
@@ -1,6 +1,13 @@
 package auth
 
-import "github.com/alexedwards/argon2id"
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+)
 
 func HashPassword(password string) (string, error) {
 	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
@@ -13,7 +20,43 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hash string) (bool, error) {
 	match, err := argon2id.ComparePasswordAndHash(password, hash)
 	if err != nil { 
-		return match, err
+		return match, fmt.Errorf("error comparing password hash: %s", err)
 	}
 	return match, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	now := time.Now()
+	claims := &jwt.RegisteredClaims{
+		IssuedAt:	jwt.NewNumericDate(now),
+		ExpiresAt:	jwt.NewNumericDate(now.Add(expiresIn)),
+		Issuer:		"chirpy",
+		Subject:	userID.String(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(tokenSecret))
+	if err != nil { 
+		return "", err
+	}
+	
+	return tokenString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := &jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil { 
+		return uuid.Nil, fmt.Errorf("validating jwt: %s", err)
+	}
+	if !token.Valid {
+		return uuid.Nil, fmt.Errorf("invalid token")
+	}
+
+	userID, err := uuid.Parse(claims.Subject)
+	if err != nil { 
+		return uuid.Nil, fmt.Errorf("error parsing id into uuid")
+	}
+	return userID, nil
 }
--- internal/auth/authentication_test.go
+++ internal/auth/authentication_test.go
@@ -2,27 +2,136 @@ package auth
 
 import (
 	"testing"
+	"time"
+
+	"github.com/google/uuid"
 )
 
-func TestHashPassword(t *testing.T) {
-	password := "p@ssword123"
-	hash, err := HashPassword(password)
+func TestHashValidatePassword(t *testing.T) {
+	password1 := "p@ssword123"
+	password2 := "someotherpassword"
+	hash1, _ := HashPassword(password1)
+	hash2, _ := HashPassword(password2)
 
-	if hash == "" || err != nil {
-		t.Errorf(`HashPassword("%s") = %s, %v, want non-empty string`, password, hash, err)
-	} 
-
-	match, err := CheckPasswordHash(password, hash)
-
-	if !match || err != nil { 
-		t.Errorf(`CheckPasswordHash("%s","%s") = %t, %v, want true`, password, hash, match, err)
+	tests := []struct {
+		name		string
+		password	string
+		hash		string
+		wantMatch	bool
+		wantErr		bool
+	}{
+		{
+			name:		"Correct password",
+			password:	password1,
+			hash:		hash1,
+			wantMatch:	true,
+			wantErr:	false,
+		},
+		{
+			name:		"Incorrect password",
+			password:	"wrongpassword",
+			hash:		hash1,
+			wantMatch:	false,
+			wantErr:	false,
+		},
+		{
+			name:		"Password doesn't match different hash",
+			password:	password1,
+			hash:		hash2,
+			wantMatch:	false,
+			wantErr:	false,
+		},
+		{
+			name:		"Empty password",
+			password:	"",
+			hash:		hash1,
+			wantMatch:	false,
+			wantErr:	false,
+		},
+		{
+			name:		"Invalid hash",
+			password:	password1,
+			hash:		"invalidhash",
+			wantErr:	true,
+			wantMatch:	false,
+		},
 	}
 
-	newPass := "123password"
-	match, err = CheckPasswordHash(newPass, hash)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			match, err := CheckPasswordHash(tt.password, tt.hash)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("HashPassword() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if match != tt.wantMatch {
+				t.Errorf("HashPassword() match = %v, wantMatch %v", match, tt.wantMatch)
+				return
+			}
+		})
 
-	if match || err != nil {
-		t.Errorf(`CheckPasswordHash("%s","%s") = %t, %v, want false`, newPass, hash, match, err)
+	}
+}
+
+func TestValidateJWT(t *testing.T) {
+	userID := uuid.New()
+	validToken, _ := MakeJWT(userID, "itsasecret", 3 * time.Second)
+
+	tests := []struct {
+		name		string
+		tokenString	string
+		tokenSecret	string
+		sleepDuration	time.Duration
+		wantUserID	uuid.UUID
+		wantErr		bool
+	}{
+		{
+			name:		"Valid token",
+			tokenString:	validToken,
+			tokenSecret:	"itsasecret",
+			sleepDuration:	0 * time.Second,
+			wantUserID:	userID,
+			wantErr:	false,
+
+		},
+		{
+			name:		"Invalid token",
+			tokenString:	"invalid.token.string",
+			tokenSecret:	"itsasecret",
+			sleepDuration:	0 * time.Second,
+			wantUserID:	uuid.Nil,
+			wantErr:	true,
+		},
+		{
+			name:		"Wrong secret",
+			tokenString:	validToken,
+			tokenSecret:	"wrongsecret",
+			sleepDuration:	0 * time.Second,
+			wantUserID:	uuid.Nil,
+			wantErr:	true,
+		},
+		{
+			name:		"Expired token",
+			tokenString:	validToken,
+			tokenSecret:	"itsasecret",
+			sleepDuration:	3 * time.Second,
+			wantUserID:	uuid.Nil,
+			wantErr:	true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			time.Sleep(tt.sleepDuration)
+			gotUserID, err := ValidateJWT(tt.tokenString, tt.tokenSecret)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateJWT() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotUserID != tt.wantUserID {
+				t.Errorf("ValidateJWT() gotUserID = %v, wantUserID %v", gotUserID, tt.wantUserID)
+			}
+		})
 	}
 }
 
